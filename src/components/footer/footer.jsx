22f38df63f8fbce44ef8a71a11475d0e1bf5a831
Adilxa <adilhaimerov82@gmail.com>
2022-03-26 18:40:14 +0600

    first commit

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,114 @@
+
+
+const Footer = () => {
+    return (
+        
+
+
+
+
+        
+<footer className="page-footer font-small blue pt-4">
+
+    <div className="container text-center text-md-left ">
+
+    <div className="row d-flex justify-content-evenly">
+
+      <div className="col-md-6 mt-md-0 mt-3">
+
+
+        <h5 className="text-uppercase">Купи собаку </h5>
+        <p><i>Lorem ipsum dolor sit amet consectetur adipisicing elit. A laborum kfjkldjfkjsdkfjdklfjkdsjfknmcnvxcjvjijdmncjzojlm
+             error labore, obcaecati magni quo dolores voluptas necessitatibus officia?</i></p>
+
+      </div>
+
+
+      <hr className="clearfix w-100 d-md-none pb-3"/>
+
+      <div className="col-md-3 mb-md-0 mb-3 ">
+
+
+        <h5 className="text-uppercase">Links</h5>
+
+        <ul className="list-unstyled ">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+
+
+     
+    
+<footer className="page-footer font-small special-color-dark pt-4">
+
+
+  <div className="container">
+
+    
+    <div className="row">
+
+      
+      <div className="col-md-6 mb-4">
+
+        
+        <form className="form-inline">
+          <input className="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search"
+            aria-label="Search"/>
+          <i className="fas fa-search" aria-hidden="true"></i>
+        </form>
+    
+
+      </div>
+      
+      <div className="col-md-6 mb-4">
+
+        <form className="input-group">
+          <input type="text" className="form-control form-control-sm" placeholder="Your email"
+            aria-label="Your email" aria-describedby="basic-addon2"/>
+          <div className="input-group-append">
+            <button className="btn btn-sm btn-outline-white my-0" type="button">Sign up</button>
+          </div>
+        </form>
+
+      </div>
+      
+
+    </div>
+
+
+  </div>
+
+
+
+
+</footer>
+
+
+    </div>
+
+
+  </div>
+ 
+
+
+</footer>
+
+
+
+        
+    );
+}
+
+export default Footer;
